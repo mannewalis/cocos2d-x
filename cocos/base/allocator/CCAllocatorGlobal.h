c566e8555e21df8fc566740a1d834cfef9e7c371
mannewalis <justinagraham@gmail.com>
2014-10-09 14:00:30 -0700

    commit rename

---
+++ cocos/base/allocator/CCAllocatorGlobal.h
@@ -0,0 +1,13 @@
+//
+//  CCAllocatorGlobal.h
+//  cocos2d_libs
+//
+//  Created by Justin Graham on 10/9/14.
+//
+//
+
+#ifndef cocos2d_libs_CCAllocatorGlobal_h
+#define cocos2d_libs_CCAllocatorGlobal_h
+
+
+#endif
