beebbe160323d629ada9c1493ce2c74c5c7cbb8f
mannewalis <justinagraham@gmail.com>
2014-11-12 13:24:48 -0800

    updated to support nested clipping that optionally intersects with the previous clipping rect.

--- cocos/renderer/CCScissorCommand.cpp
+++ cocos/renderer/CCScissorCommand.cpp
@@ -11,22 +11,45 @@
 
 NS_CC_BEGIN
 
+int ScissorCommand::_scissorCount = 0;
+Rect ScissorCommand::_currentClippingRegion;
+
 BeginScissorCommand::BeginScissorCommand()
 {
     _type = RenderCommand::Type::BEGIN_SCISSOR_COMMAND;
 }
 
-void BeginScissorCommand::init(float depth, const Rect& clippingRegion)
+void BeginScissorCommand::init(float depth, const Rect& clippingRegion, bool respectPreviousScissor)
 {
     _globalOrder = depth;
     _clippingRegion = clippingRegion;
+    
+    if (0 < _scissorCount)
+        _previousClippingRegion = _currentClippingRegion;
+    
+    if (respectPreviousScissor)
+    {
+        //set the intersection of _parentScissorRect and frame as the new scissor rect
+        if (_clippingRegion.intersectsRect(_previousClippingRegion))
+        {
+            float x = MAX(_clippingRegion.origin.x, _previousClippingRegion.origin.x);
+            float y = MAX(_clippingRegion.origin.y, _previousClippingRegion.origin.y);
+            float xx = MIN(_clippingRegion.origin.x+_clippingRegion.size.width, _previousClippingRegion.origin.x+_previousClippingRegion.size.width);
+            float yy = MIN(_clippingRegion.origin.y+_clippingRegion.size.height, _previousClippingRegion.origin.y+_previousClippingRegion.size.height);
+            _clippingRegion = Rect(x, y, xx-x, yy-y);
+        }
+    }
+    
+    _currentClippingRegion = _clippingRegion;
 }
 
 void BeginScissorCommand::execute()
 {
-    glEnable(GL_SCISSOR_TEST);
+    if (0 == _scissorCount)
+        glEnable(GL_SCISSOR_TEST);
     GLView* glView = Director::getInstance()->getOpenGLView();
     glView->setScissorInPoints(_clippingRegion.origin.x, _clippingRegion.origin.y, _clippingRegion.size.width, _clippingRegion.size.height);
+    ++_scissorCount;
 }
 
 
@@ -42,7 +65,15 @@ void EndScissorCommand::init(float depth)
 
 void EndScissorCommand::execute()
 {
-    glDisable(GL_SCISSOR_TEST);    
+    --_scissorCount;
+    if (0 == _scissorCount)
+        glDisable(GL_SCISSOR_TEST);
+    else
+    {
+        GLView* glView = Director::getInstance()->getOpenGLView();
+        glView->setScissorInPoints(_previousClippingRegion.origin.x, _previousClippingRegion.origin.y, _previousClippingRegion.size.width, _previousClippingRegion.size.height);
+        _currentClippingRegion = _previousClippingRegion;
+    }
 }
 
 NS_CC_END
--- cocos/renderer/CCScissorCommand.h
+++ cocos/renderer/CCScissorCommand.h
@@ -29,25 +29,52 @@
 
 NS_CC_BEGIN
 
+// @brief base class for scissor commands, provides a static count
+// of the nested scissor commands that have begun. Scissor state
+// is only disabled once the count reaches zero.
+class ScissorCommand
+    : public RenderCommand
+{
+public:
+    
+    static int scissorCount() 
+    {
+        return _scissorCount;
+    }
+    
+protected:
+    
+    // @brief the scissor clipping region for counts > 0.
+    Rect _clippingRegion;
+    
+    // @brief the previous clipping region.
+    Rect _previousClippingRegion;
+
+    // @brief tracks the number of nested scissor commands,
+    // scissor is disabled when all commands have completed.
+    static int _scissorCount;
+    
+    // @brief previous clipping region
+    static Rect _currentClippingRegion;
+};
+
 // @brief increments the scissor count and enables the scissor
 // if this is the first begin. It also remembers the old scissor
 // rectangle, and will restore it when it ends.
 class BeginScissorCommand
-    : public RenderCommand
+    : public ScissorCommand
 {
 public:
     BeginScissorCommand();
-    void init(float depth, const Rect& clippingRegion);
+    void init(float depth, const Rect& clippingRegion, bool respectPreviousScissor = false);
     void execute();
-protected:
-    Rect _clippingRegion;
 };
 
 // @brief ends a scissor command, restores the previous scissor
 // state if any, and decrements the scissor count. If the scissor
 // count reaches zero, then the scissor is disabled.
 class EndScissorCommand
-    : public RenderCommand
+    : public ScissorCommand
 {
 public:
     EndScissorCommand();
